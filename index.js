4ee4bc534467fdb7b811a3e7cf29ba1d3de969e9
cosmicelijah <cosmicelijah@gmail.com>
2021-01-20 18:15:28 -0500

    make it better

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Coconut is ready to go!');
+});
+
+client.login('ODAxNDM5MjI1MjY5NzE0OTc0.YAgscQ._XYp_6pbEA9AgCoGuSz2359CWUU');
