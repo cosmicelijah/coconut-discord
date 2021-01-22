c927db47f61810bac0d2898b79bd56be77663ba4
cosmicelijah <cosmicelijah@gmail.com>
2021-01-21 20:42:21 -0500

    1.11 rc10

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ client.login(config.token);
 
 client.commands = new Discord.Collection();
 
-client.events = new Discord.Collection();
+//client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(client, Discord);
-})
+});
+
+client.on("ready", async() => {
+
+console.log('Coconut is ready!');
+
+    client.user.setPresence({
+        status: status.active,
+        game: {
+            name: status.activity,
+            type: status.activityType
+        }
+    });
+});
+
+if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const cmd = args.shift().toLowerCase();
+
+const command = client.commands.get(cmd);
+
+if(command) command.execute(client, message, args, Discord);
