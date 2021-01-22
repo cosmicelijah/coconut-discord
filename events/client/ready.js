53244e5cd24bebcbf2f3f30a4de9223baf7be5bf
cosmicelijah <cosmicelijah@gmail.com>
2021-01-21 20:33:34 -0500

    1.11 rc9

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,16 @@
+const status = require('/app/status.json')
+
 module.exports = () =>{
     console.log('Coconut is ready!');
+
+    client.on("ready", async() => {
+        client.user.setPresence({
+            status: status.active,
+            game: {
+                name: status.activity,
+                type: status.activityType
+            }
+        });
+    });
+
 }
--- index.js
+++ index.js
@@ -14,14 +14,3 @@ client.events = new Discord.Collection();
 ['command_handler', 'event_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(client, Discord);
 })
-
-//Sets the bots Presence and Activity. Uses Data in status.json.
-client.on("ready", async() => {
-    client.user.setPresence({
-        status: status.active,
-        game: {
-            name: status.activity,
-            type: status.activityType
-        }
-    });
-});
