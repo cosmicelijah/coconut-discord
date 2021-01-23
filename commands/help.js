bb3d0e7c73e41939fc311e59c778ecf1c19562c6
cosmicelijah <cosmicelijah@gmail.com>
2021-01-22 22:28:39 -0500

    1.13 rc4.1

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
                 {name: "Prefix to activate me", value: 'Use a "?" to call me, but you already know that nya~'},
                 {name: "Ping", value: `${commandPing.description}`},
                 {name: "Help", value: "Help"},
-                {name: "Time", value: `${commandTime.description}`}
+                {name: "Time", value: `${commandTime.description}`},
                 {name: "Coming soon!", value: "More features coming soon, so keep your eyes peeled!"}
             )
             .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fm-anime.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F9df78eab33525d08d6e5fb8d27136e95%2FN%2Fe%2FNekopara_Coconut_Maid_Dress_1.jpg&f=1&nofb=1');
