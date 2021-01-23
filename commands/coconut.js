9d60d0e3d659fb3d1a6ce1b4442fa705d05e142b
cosmicelijah <cosmicelijah@gmail.com>
2021-01-23 00:06:36 -0500

    1.13 rc10.1

--- commands/coconut.js
+++ commands/coconut.js
@@ -16,7 +16,7 @@ module.exports = {
                 .setColor('#1fdd94')
                 .setAuthor('Coconut here,')
                 .setTitle("Here's the image you requested nya~")
-                .setImage(`${selectImage}`);
+                .setImage('https://cdn.discordapp.com/attachments/787793633943748613/801705812778942474/F21CFCD2-3B26-4B17-8A96-096D39B10C68.JPG');
             message.channel.send(coconutEmbed);
     }
 }
