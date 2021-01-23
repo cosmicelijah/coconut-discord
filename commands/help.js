cf73dda8602d0b033ea3d66bb289ddd882c05cd7
cosmicelijah <cosmicelijah@gmail.com>
2021-01-22 23:13:42 -0500

    1.13 rc5

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 const { Channel } = require("discord.js");
 const commandPing = require("./ping.js")
 const commandTime = require("./time.js")
+const commandCoconut = require('./coconut.js')
 module.exports = {
     name: 'help',
     description: "list of available commands",
@@ -12,8 +13,9 @@ module.exports = {
             .addFields(
                 {name: "Prefix to activate me", value: 'Use a "?" to call me, but you already know that nya~'},
                 {name: "Ping", value: `${commandPing.description}`},
-                {name: "Help", value: "Help"},
+                {name: "Help", value: "list of available commands"},
                 {name: "Time", value: `${commandTime.description}`},
+                {name: "Coconut", value: `${commandCoconut.description}`},
                 {name: "Coming soon!", value: "More features coming soon, so keep your eyes peeled!"}
             )
             .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fm-anime.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F9df78eab33525d08d6e5fb8d27136e95%2FN%2Fe%2FNekopara_Coconut_Maid_Dress_1.jpg&f=1&nofb=1');
