a2e4628072238a5799ca13637fc80a55c1201b86
cosmicelijah <cosmicelijah@gmail.com>
2021-01-23 23:19:26 -0500

    1.14 rc1

--- commands/coconut.js
+++ commands/coconut.js
@@ -1,4 +1,5 @@
 const imageDatabase = require("/app/data/imageDatabase.json")
+const gifDatabase = require("/app/data/gifDatabase.json")
 const coconutArray = []
 const cultureAmount = 51
 for (i = 0; i < cultureAmount; i++) {
@@ -8,14 +9,18 @@ module.exports = {
     name: 'coconut',
     description: "Coconut images (may contain the S P I C Y)",
     execute(client, message, args, Discord){
-        const keys = Object.keys(coconutArray)
-        const randIndex = Math.floor(Math.random() * keys.length)
-        const randKey = keys[randIndex]
-        var selectImage = imageDatabase[randKey]
-            let coconutEmbed = new Discord.MessageEmbed()
-                .setColor('#1fdd94')
-                .setTitle("Here's the image you requested nya~")
-                .setImage(`${selectImage}`);
-            message.channel.send(coconutEmbed);
-    }
+        if(args[0] == "bruh") {
+            const keys = Object.keys(coconutArray)
+            const randIndex = Math.floor(Math.random() * keys.length)
+            const randKey = keys[randIndex]
+            var selectGif = gifDatabase[randKey]
+            var selectImage = imageDatabase[randKey]
+                let coconutEmbed = new Discord.MessageEmbed()
+                    .setColor('#1fdd94')
+                    .setTitle("Here's the image you requested nya~")
+                    .setImage(`${selectImage}`);
+                message.channel.send(coconutEmbed);
+        } else if(args[1] == "hurb") {
+            message.channel.send("no");
+        };
 }
