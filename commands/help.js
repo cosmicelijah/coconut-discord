d24ea20381f567bc08e53801c3c8f2b97522e97e
cosmicelijah <cosmicelijah@gmail.com>
2021-02-02 20:59:07 -0500

    Added repeat command

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ const { Channel } = require("discord.js");
 const commandPing = require("./ping.js")
 const commandTime = require("./time.js")
 const commandCoconut = require('./coconut.js')
+const commandRepeat = require('./repeat.js')
 module.exports = {
     name: 'help',
     description: "list of available commands",
@@ -16,6 +17,7 @@ module.exports = {
                 {name: "Help", value: "list of available commands"},
                 {name: "Time", value: `${commandTime.description}`},
                 {name: "Coconut", value: `${commandCoconut.description}`},
+                {name: "Repeat", value: `${commandRepeat.description}`},
                 {name: "Coming soon!", value: "More features coming soon, so keep your eyes peeled!"}
             )
             .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fm-anime.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F9df78eab33525d08d6e5fb8d27136e95%2FN%2Fe%2FNekopara_Coconut_Maid_Dress_1.jpg&f=1&nofb=1');
---
+++ commands/repeat.js
@@ -0,0 +1,5 @@
+module.exports = {
+    name: "repeat",
+    description: "Repeats everything the user says after the command",
+    
+}
