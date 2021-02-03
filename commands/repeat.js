363774a1f974c334c23c6a5b4eb75773aa036a67
cosmicelijah <cosmicelijah@gmail.com>
2021-02-02 20:59:12 -0500

    Added repeat command

--- commands/repeat.js
+++ commands/repeat.js
@@ -1,5 +1,11 @@
 module.exports = {
     name: "repeat",
     description: "Repeats everything the user says after the command",
-    
+    execute(client, message, args, Discord) {
+        if(args.length) {
+            let repeatEmbed = new Discord.MessageEmbed()
+                .setTitle(`${args.length}`);
+            message.channel.send(repeatEmbed);
+        }
+    }
 }
