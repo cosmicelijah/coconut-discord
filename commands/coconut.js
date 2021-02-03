57a0d8e6a7ed3bf7b6f62454c2a8555cce268fec
cosmicelijah <cosmicelijah@gmail.com>
2021-02-02 20:14:12 -0500

    Testing dumpLoop w/ embeds but no nested loops :(

--- commands/coconut.js
+++ commands/coconut.js
@@ -51,15 +51,8 @@ module.exports = {
         } else if(args[0] === "image") {
             if(args[1] === "dumpass") {
                 var dumpNumber = args[2]
-                message.channel.send(`${dumpNumber}`);
+                // message.channel.send(`${dumpNumber}`);
                 for (d = 0; d < dumpNumber; d++) {
-
-                    for (i = 0; i < cultureAmount; i++) {
-                        coconutArray.push(i);
-                    };
-                    
-                    const coconutGifArray = []
-
                     let coconutImageEmbed = new Discord.MessageEmbed()
                         .setColor('#1fdd94')
                         .setTitle("Here's the image you requested nya~")
