a5053574d01982780c19b68694e322aee0167230
cosmicelijah <cosmicelijah@gmail.com>
2021-02-13 00:13:08 -0500

    Carl is rebarded

---
+++ commands/roleadd.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'roleadd',
+    description: "Carl is rebarded",
+    execute(client, message, args, Discord){
+        //message.member.roles.add('575502501500026953');
+        message.channel.send("What a fucking retardass, here isn't the role dipshit.");
+    }
+}
