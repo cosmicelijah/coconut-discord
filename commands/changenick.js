e2f83992a824899a92292682ae311aa14e8a3153
cosmicelijah <76886020+cosmicelijah@users.noreply.github.com>
2021-02-19 07:37:55 -0500

    Create changenick.js

---
+++ commands/changenick.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'changenick',
+    description: "Shooting Avery off his high horse with bots.",
+    execute(client, message, args, Discord){
+        let args = []
+        message.guild.id(406466447565520906).setNickname(`args[0]`)
