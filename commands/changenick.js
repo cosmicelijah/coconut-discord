beec2a14556397682b4252cdad61846798b49525
cosmicelijah <76886020+cosmicelijah@users.noreply.github.com>
2021-02-19 07:41:58 -0500

    Update changenick.js

--- commands/changenick.js
+++ commands/changenick.js
@@ -2,5 +2,8 @@ module.exports = {
     name: 'changenick',
     description: "Shooting Avery off his high horse with bots.",
     execute(client, message, args, Discord){
-        let args = []
-        message.guild.id(406466447565520906).setNickname(`args[0]`)
+        let changeNick = args[0]
+        message.guild.id(406466447565520906).setNickname(changeNick);
+        message.channel.send('Done!');
+    }
+}
