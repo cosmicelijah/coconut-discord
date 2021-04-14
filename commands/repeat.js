44a8474b82ed50a644c6805143ea918f3a1fc7d6
Elijah <cosmicelijah@gmail.com>
2021-04-14 15:14:41 -0400

    fixing repeat

--- commands/repeat.js
+++ commands/repeat.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(client, message, args, Discord) {
         if(args.length) {
             var repeatArgs = args[args.length];
-            message.channel.send(repeatArgs);
+            message.channel.send("pp time");
             // let repeatEmbed = new Discord.MessageEmbed()
             //     .setTitle(repeatArgs)
             //     .setColor("#423879");
