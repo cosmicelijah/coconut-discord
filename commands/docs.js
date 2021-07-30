e4b52d9d67dcecbf2a457ff179a7e5e9e899b64d
cosmicelijah <cosmicelijah@gmail.com>
2021-07-30 15:19:45 -0400

    Improved counter on coconut command

--- commands/docs.js
+++ commands/docs.js
@@ -9,8 +9,8 @@ module.exports = {
                     .setColor('#00FFFF')
                     .addFields(
                         {name: "Thursday July 29th, 2021", value: "Got sudden inspiration to begin working on this bot again. I have a few goals such as improving the way the image database is handled, making code more optimized, and other general back end stuff."},
-                        {name: 'Friday July 30th, 2021', value: 'Began making some improvements, ran into errors, cried. Decided to start making docs so I have memories of this experience forever (or as long as the bot\'s source code exists)'}    
-                    )
+                        {name: 'Friday July 30th, 2021', value: 'Began making some improvements, ran into errors, cried. Decided to start making docs so I have memories of this experience forever (or as long as the bot\'s source code exists)\nLater that day, I vastly improved the code that chooses the image/gif in the ?coconut command. I feel like it runs a lot faster now. Plans to add an arg handler are underway.'}
+                        )
                     .setThumbnail('https://cdn.discordapp.com/attachments/870716191147163648/870716229579575376/unknown.png');
                 message.channel.send(doc1Embed);
                 break;
