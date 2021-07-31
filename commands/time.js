a6ae7a7e02a2e4fb53d4438165995eee0f215f14
cosmicelijah <cosmicelijah@gmail.com>
2021-07-30 22:29:43 -0400

    fixing help command again

--- commands/time.js
+++ commands/time.js
@@ -6,7 +6,7 @@ module.exports = {
         let timeEmbed = new Discord.MessageEmbed()
             .setColor('#890674')
             .addFields(
-                { name: "What time is it?", value: `It's time for you to get a watch!\nIt's also ${timeDate}` }
+                { name: "What time is it?", value: `It's time for you to get a watch!\nIt's also ${timeDate}`}
             );
         message.channel.send(timeEmbed);
     }
